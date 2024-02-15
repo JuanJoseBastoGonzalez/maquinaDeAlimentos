b1284e04fc3147d0f28066e5cf40c683af98c129
JuanJoseBastoGonzalez <151982381+JuanJoseBastoGonzalez@users.noreply.github.com.>
2024-02-14 20:34:07 -0500

    feat: :tada: edicion de espacios
    espacios editados

--- js/java.js
+++ js/java.js
@@ -3,35 +3,23 @@ const precios = {
   B: 340,
   C: 390
 };
-
 const monedas = [100, 50, 10];
-
-// Función principal
 function comprarProducto() {
-  let producto = prompt("Seleccione producto: A, B, C").toUpperCase();
-
-  // Verificar si el producto ingresado es válido
+  let producto = prompt("Seleccione su producto: A, B, C").toUpperCase();
   if (!precios.hasOwnProperty(producto)) {
     alert("Producto no valido.");
     return;
   }
-
   let montoIngresado = 0;
   let vuelto = [];
-
-
   while (montoIngresado < precios[producto]) {
-    let monto = parseInt(prompt("Ingrese moneda (10, 50, 100)  "));
-    
+    let monto = parseInt(prompt("Ingrese el valor de su moneda (10, 50, 100)  "));
     if (!monedas.includes(monto)) {
       alert("Moneda  no permitida.");
       continue;
     }
-
     montoIngresado += monto;
   }
-
-
   let cambio = montoIngresado - precios[producto];
   for (let i = 0; i < monedas.length; i++) {
     while (cambio >= monedas[i]) {
@@ -39,8 +27,6 @@ function comprarProducto() {
       cambio -= monedas[i];
     }
   }
-
   alert(`Su restante es: ${vuelto.join(' ')}`);
 }
-
 comprarProducto();
